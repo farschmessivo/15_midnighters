8601b5bbafe839933b994e1c894a255764be2d91
Roman Ch <hello@romangagarin.com>
2018-02-15 08:12:31 +0000

    change load_solution_attempts

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -7,35 +7,35 @@ from collections import Counter
 def load_api_data(page_number):
     params = {'page': page_number}
     raw_data_response = requests.get(
-        'http://devman.org/api/challenges/solution_attempts/', params)
+        'http://devman.org/api/challenges/solution_attempts/',
+        params
+    )
     json_data = raw_data_response.json()
     return json_data
 
 
 def load_solution_attempts(data_loader):
-    solution_attempts = []
-    first_page_number = 1
-    first_page_data = data_loader(first_page_number)
-    total_number_of_pages = first_page_data['number_of_pages']
-    solution_attempts.extend(first_page_data['records'])
-
-    for page_number in range(first_page_number+1, total_number_of_pages+1):
-        raw_data = data_loader(page_number)
-        solution_attempts.extend(raw_data['records'])
-
-    return solution_attempts
+    page = 1
+    while True:
+        json_data = data_loader(page)
+        for attempt in json_data['records']:
+            yield attempt
+        page += 1
+        if page > json_data['number_of_pages']:
+            break
 
 
 def calc_midnighters_top_list(midnight_attempts_usernames):
     midnighters_top_list = Counter(midnight_attempts_usernames).most_common()
-
     return midnighters_top_list
 
 
 def is_midnight_attempt(attempt):
     local_timezone = pytz.timezone(attempt['timezone'])
-    attempt_localized_datetime = datetime.datetime.fromtimestamp(attempt['timestamp'],
-                                                     local_timezone)
+    attempt_localized_datetime = datetime.datetime.fromtimestamp(
+        attempt['timestamp'],
+        local_timezone
+    )
     local_time = attempt_localized_datetime.time()
     lower_bound = datetime.time(0, 0, 0)
     upper_bound = datetime.time(4, 0, 0)
@@ -43,10 +43,12 @@ def is_midnight_attempt(attempt):
     return lower_bound <= local_time < upper_bound
 
 
-def get_midnight_attempts_usernames(solution_attempts, filter_function):
-    midnighters = [attempt['username'] for attempt in solution_attempts
-                   if filter_function(attempt)]
-    return midnighters
+def get_filtered_attempts_usernames(solution_attempts, filter_function):
+    filtered_usernames = [
+        attempt['username'] for attempt in solution_attempts
+        if filter_function(attempt)
+    ]
+    return filtered_usernames
 
 
 def print_to_console(midnighters_top_list):
@@ -54,12 +56,15 @@ def print_to_console(midnighters_top_list):
     for index, (username, num_of_midnight_attempts) in enumerate(
             midnighters_top_list, 1):
         print('{}. {} made {} midnight attempts.'.format(
-            index, username, num_of_midnight_attempts))
+            index,
+            username,
+            num_of_midnight_attempts)
+        )
 
 
 if __name__ == '__main__':
     solution_attempts_list = load_solution_attempts(load_api_data)
-    midnight_attempts_usernames = get_midnight_attempts_usernames(
+    midnight_attempts_usernames = get_filtered_attempts_usernames(
         solution_attempts_list, is_midnight_attempt)
     midnighters_top_list = calc_midnighters_top_list(
         midnight_attempts_usernames)
